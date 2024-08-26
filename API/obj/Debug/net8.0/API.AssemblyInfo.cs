4868e9b2b5249e4f147e993aee7b395912e31f7d
Viv-dn <Vivian96@live.se>
2024-08-26 11:43:14 +0200

    rm obj

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81cb20c408e850ca3e6e292c6794230115308277")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad496e6890e0a05fe4bc005845c28e129eacdecb")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
