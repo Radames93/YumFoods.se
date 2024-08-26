d730500eba91ae845bbd4d5300ce86c4d4257f6f
Fredrik Rask-Andersen <113111789+fRaskA@users.noreply.github.com>
2024-08-26 13:18:27 +0200

    added UserRepository.cs and added property for password in User.cs

--- DataAccess/Entities/Product.cs
+++ DataAccess/Entities/Product.cs
@@ -1,15 +1,13 @@
-﻿namespace DataAccess.Entities
+﻿namespace DataAccess.Entities;
+public class Product
 {
-    public class Product
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public double Price { get; set; }
-        public string Category { get; set; }
-        public string Diet { get; set; }
-        public string ImgRef { get; set; }
-        public string Description { get; set; }
-        public string Ingredients { get; set; }
-        public string Cuisine { get; set; }
-    }
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public double Price { get; set; }
+    public string Category { get; set; }
+    public string Diet { get; set; }
+    public string ImgRef { get; set; }
+    public string Description { get; set; }
+    public string Ingredients { get; set; }
+    public string Cuisine { get; set; }
 }
