ad496e6890e0a05fe4bc005845c28e129eacdecb
Viv-dn <Vivian96@live.se>
2024-08-26 11:30:59 +0200

    Created Yumfoods db context

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b141af51f306f010b1c9a6ba3dfdab0a04340e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81cb20c408e850ca3e6e292c6794230115308277")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ DataAccess/YumFoodsDb.cs
@@ -0,0 +1,12 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess;
+
+public class YumFoodsDb : DbContext
+{
+    public YumFoodsDb(DbContextOptions options) : base(options)
+    {
+        
+    }
+}
