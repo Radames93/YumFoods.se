0f2678e9c8ef1c8a86b4a1f1ba1ef052bac68211
Viv-dn <Vivian96@live.se>
2024-08-26 11:04:16 +0200

    api assembly cashe

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+694356ff24567318791e0e20562ca62a846ddfd9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba07e82a88feebc5347d1ff0cae5708ae0a950ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
