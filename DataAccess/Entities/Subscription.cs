a0f2371e27ba4fc9f43c8ce1c3065c77f865e059
Geweria <geweria.negash@iths.se>
2024-08-26 13:27:10 +0200

    fixed duplication in dbcontext

--- DataAccess/Entities/Subscription.cs
+++ DataAccess/Entities/Subscription.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Entities;
 
-internal class Subsciption
+internal class Subscription
 {
     public int Id { get; set; }
     public string Title { get; set; }
