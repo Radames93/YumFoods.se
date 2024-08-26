5bb4af35102fcecc8bed550bd831d64e7d560900
Geweria <geweria.negash@iths.se>
2024-08-26 14:50:26 +0200

    corrected variable

--- API/Program.cs
+++ API/Program.cs
@@ -1,17 +1,19 @@
+using DataAccess;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
 
-var connectionString = Environment.GetEnvironmentVariable("YumFoodsConnectionString");
+var connectionString = Environment.GetEnvironmentVariable("YumFoodsDbConnectionString");
+var connectionString2 = Environment.GetEnvironmentVariable("YumFoodsUserDbConnectionString");
 
-// Add services to the container
-builder.Services.AddDbContext<DbContext>(options =>
+builder.Services.AddDbContext<YumFoodsDb>(options =>
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
+builder.Services.AddDbContext<YumFoodsUserDb>(options =>
+    options.UseMySql(connectionString2, ServerVersion.AutoDetect(connectionString2)));
 
 var app = builder.Build();
 
