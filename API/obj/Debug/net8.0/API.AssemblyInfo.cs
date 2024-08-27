fbba4064dae819eb8148d4c21366c103827a3ed1
Geweria <geweria.negash@iths.se>
2024-08-27 14:26:21 +0200

    Prep

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5bbed3f7686dbc69fdfa8319db991dafa0696ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+276b7ab8fdaa148412d8a9639500c5acbbda9bef")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
