04438e5ed181aeb435792f0e847b61fd95516b0f
Viv-dn <Vivian96@live.se>
2024-08-27 10:58:46 +0200

    conflicts

--- DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
+++ DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d730500eba91ae845bbd4d5300ce86c4d4257f6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2af8e65b488e47bd1c01a9cf40afbd8806a43a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
