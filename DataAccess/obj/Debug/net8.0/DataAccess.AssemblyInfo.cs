b9628f17fe34b92c6ee3188a1f9b2361b1279950
Geweria <geweria.negash@iths.se>
2024-08-27 11:35:12 +0200

    Staged conflicting files

--- DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
+++ DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6577f065f76df68251620c2a79037ff12610a10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e953d9477b4a528df93fa0611c550e40311ea1f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
