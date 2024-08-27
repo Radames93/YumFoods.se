185c133760b7fecd0c79e38f81e398b1d1c68f98
Viv-dn <Vivian96@live.se>
2024-08-27 16:40:31 +0200

    stashed conflicting files for a merge

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d46bdbc6361be6c0e5e4c1fd953938093d8a2f81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df8c20917684e4d8aa5fc251401223c486b36288")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
