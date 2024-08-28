88cf767f2fd8d8c9aee069dafcf8a83e955fa5b0
Viv-dn <Vivian96@live.se>
2024-08-28 11:39:48 +0200

    stashed cinflicting files for a switch

--- DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
+++ DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ba1b7e55ad1d9f82cb8601ec39fafa515c02fea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e296759362fb2c9e6504edb187a2270fb1b26a62")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
