d2c5a6c5f7cc10353e394550105eaa2f21003200
Geweria <geweria.negash@iths.se>
2024-08-28 13:58:48 +0200

    moved entity to shared project

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d346bbb4677914b7b4c19532ba8e8534c17e4d91")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b8b0cfabd2a07b2fbc547be67181e560901c87b")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
