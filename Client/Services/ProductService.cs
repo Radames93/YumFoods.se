4ac64b269301211d8e4fe6af7e1ec8eb789a01a8
Viv-dn <Vivian96@live.se>
2024-08-29 16:12:47 +0200

    Started on product service on client side

--- Client/Services/ProductService.cs
+++ Client/Services/ProductService.cs
@@ -1,12 +1,76 @@
-﻿namespace Shared.Services;
+﻿using Shared.DTOs;
+using Shared.Entities;
+using Shared.Interfaces;
 
-public class ProductService 
+namespace Shared.Services;
+
+public class ProductService : IProductRepository
 {
     private readonly HttpClient _httpClient;
 
     ///måste addHttpClient i program.cs i frontend först 
-    //public ProductService(IHttpClientFactory factory)
-    //{
-    //    _httpClient = factory.CreateClient("")
-    //}
+    public ProductService(IHttpClientFactory factory)
+    {
+        _httpClient = factory.CreateClient("YumFoodsConnectionString");
+    }
+
+
+    public async Task<List<Product>> GetAllProductsAsync()
+    {
+        var response = await _httpClient.GetAsync("products");
+        if (!response.IsSuccessStatusCode)
+        {
+            return (List<Product>)Enumerable.Empty<Product>();
+        }
+
+        var result = await response.Content.ReadFromJsonAsync<List<Product>>();
+        return result ?? (List<Product>)Enumerable.Empty<Product>();
+    }
+
+    public async Task<Product?> GetProductByIdAsync(int id)
+    {
+        var response = await _httpClient.GetAsync($"products/{id}");
+        if (!response.IsSuccessStatusCode)
+        {
+            return null;
+        }
+
+        var result = await response.Content.ReadFromJsonAsync<Product>();
+        return result;
+    }
+
+    public async Task<List<Product?>> GetProductByCategoryAsync(string category)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<List<Product?>> GetProductByCuisineAsync(string cuisine)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task AddProductAsync(Product newProd)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task UpdateProductAsync(int existingProdId, Product updatedProd)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task DeleteProductAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Product?> GetProductByNameAsync(string title)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<List<Product?>> GetProductsByDietAsync(string diet)
+    {
+        throw new NotImplementedException();
+    }
 }
