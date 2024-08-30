d6c3dcb250a4358a132f81b148d710a1183c2341
Viv-dn <Vivian96@live.se>
2024-08-30 13:17:12 +0200

    program.cs

--- Client/Program.cs
+++ Client/Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-//gör om, hämta mijövariabel???
+//gör om, hämta mijövariabel?????
 
 
 builder.Services.AddHttpClient("YumFoodsConnectionString",
