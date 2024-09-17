fa85919d8a506c4726f2eaf3bbb4207c4595f681
Viv-dn <Vivian96@live.se>
2024-09-17 10:58:10 +0200

    deleted code in program.cs client side thats not nedded

--- Client.YumFoods/Program.cs
+++ Client.YumFoods/Program.cs
@@ -1,19 +1,6 @@
- using Microsoft.AspNetCore.Cors.Infrastructure;
-using Shared.DTOs;
-using Shared.Interfaces;
-using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
-
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
-
-
-
-//app.Run(async (context) =>
-//{
-//    context.Response.ContentType = "text/html";
-//    await context.Response.SendFileAsync(Path.Combine(WebRootPath, "index.html"));
-//});
 
 
 app.UseHttpsRedirection();
