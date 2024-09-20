28281066ea753ad156dab26dbfc46c6087e6b41d
Viv-dn <Vivian96@live.se>
2024-09-20 09:45:11 +0200

    created api extension for orderanddetail

---
+++ API/Extensions/OrderAndDetailExtension.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shared.Entities;
+using Shared.Interfaces;
+
+namespace API.Extensions
+{
+    public static class OrderAndDetailExtension
+    {
+        public static IEndpointRouteBuilder MapOrderAndDetailsEndpoints(this IEndpointRouteBuilder app)
+        {
+            var group = app.MapGroup("/orderAndDetails");
+
+            group.MapPost("/", CreateOrderAndDetailAsync);
+
+            return app;
+        }
+
+        private static async Task<IResult> CreateOrderAndDetailAsync(IOrderRepository<Order> orderRepository,
+            IOrderDetailRepository<OrderDetail> detailRepository, [FromBody] OrderAndDetail request)
+        {
+            var order = new Order
+            {
+                Id = request.Order.Id,
+                UserId = request.Order.UserId,
+                OrderDate = DateTime.UtcNow,
+                DeliveryDate = request.Order.DeliveryDate,
+                Total = request.Order.Total,
+                PaymentMethod = request.Order.PaymentMethod,
+                Products = request.Order.Products
+            };
+            try
+            {
+                await orderRepository.AddOrderAsync(order);
+            }
+            catch (Exception)
+            {
+                return Results.BadRequest($"Failed to create order");
+                throw;
+            }
+
+            var orderDetail = new OrderDetail
+            {
+                OrderId = order.Id,
+                DeliveryAdress = request.OrderDetail.DeliveryAdress,
+                DeliveryCity = request.OrderDetail.DeliveryCity,
+                DeliveryPostalCode = request.OrderDetail.DeliveryPostalCode,
+                DeliveryCountry = request.OrderDetail.DeliveryCountry
+            };
+            try
+            {
+                await detailRepository.AddOrderDetailAsync(orderDetail);
+            }
+            catch (Exception)
+            {
+                try
+                {
+                    await orderRepository.DeleteOrderAsync(order.Id);
+                }
+                catch (Exception)
+                {
+                    return Results.BadRequest($"Failed to create order detail and failed to rollback order.");
+                }
+
+                return Results.BadRequest($"Could not create order detail. Order rolled back.");
+            }
+
+            return Results.Ok(order.Id);
+        }
+    }
+}
