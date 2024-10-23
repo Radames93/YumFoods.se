2500833af8e9864cb688186ae9300dea16144b8f
Ali Kathem <ali.kathem@iths.se>
2024-10-23 11:21:34 +0200

    update

--- Client.YumFoods/wwwroot/js/main.js
+++ Client.YumFoods/wwwroot/js/main.js
@@ -5955,20 +5955,17 @@ async function redirectToStripeCheckout() {
     });
 
     // Create a POST request to your backend endpoint to create the Stripe checkout session
-    //const response = await fetch("https://localhost:7216/payments", {
-      const API_KEY = variables();
-      const response = await fetch(`https://${API_KEY}/payments`, {
+    const response = await fetch("https://localhost:7216/payments", {
+      //const API_KEY = variables();
+      //const response = await fetch(`https://${API_KEY}/payments`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        //successPaymentUrl: "https://localhost:7023/payment_success.html",
-        //cancelPaymentUrl: "https://localhost:7023/payment_cancel.html",
-        successPaymentUrl:
-          "https://yumfoodsdev.azurewebsites.net/payment_success.html",
-        cancelPaymentUrl:
-          "https://yumfoodsdev.azurewebsites.net/payment_cancel.html",
+        successPaymentUrl: "https://localhost:7023/payment_success.html",
+        ancelPaymentUrl: "https://localhost:7023/payment_cancel.html",
+        //
         products: products, // Send the products array
       }),
     });
